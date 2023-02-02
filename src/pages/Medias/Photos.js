324cd82efdd3cb8e19998f08884a1e37ecdfd30e
Agnes <almoha@gmail.com>
2023-02-02 16:59:50 +0100

    carousel fonctionnel dans la gallerie

--- src/pages/Medias/Photos.js
+++ src/pages/Medias/Photos.js
@@ -2,13 +2,48 @@ import React, { useEffect } from "react";
 import bg from "../../assets/bg_2.jpg";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import Card from "../../components/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTimes,
+  faArrowLeft,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { getGallery } from "../../utils/fetchPhotos";
 import { useState } from "react";
 
-
 const Photos = () => {
   const [gallery, setGallery] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  const handleClick = (i) => {
+    if(window.innerWidth > 650){
+    setIndex(i);
+    setShowOverlay(true);
+    }
+  };
+
+  const handleClose = () => {
+    setShowOverlay(false);
+  };
+
+  const handlePrevious = () => {
+    setIndex((index + gallery.length - 1) % gallery.length);
+  };
+
+  const handleNext = () => {
+    setIndex((index + 1) % gallery.length);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        setShowOverlay(false);
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [showOverlay]);
 
   useEffect(() => {
     const loadData = async () => {
@@ -23,6 +58,13 @@ const Photos = () => {
   }, [gallery.length]);
 
   console.log(gallery);
+
+  const modalStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
 
   return (
     <>
@@ -39,9 +81,49 @@ const Photos = () => {
           <div className=" sm:w-10/12 flex justify-center bg-neutral-800 sm:py-16 p-4 z-10">
             <div className="sm:ml-1/12 sm:columns-2 lg:columns-3 xl:columns-4 sm:gap-8">
               {gallery &&
-                gallery?.map((elt) => (
-                  <Card gallery={elt?.gallery.photo} key={elt.key} />
+                gallery.map((image, i) => (
+                  <img
+                    key={i}
+                    src={image.gallery.photo}
+                    alt={image.alt}
+                    className="w-full  sm:w-80 mb-8 rounded-lg shadow-md shadow-white cursor-pointer"
+                    onClick={() => handleClick(i)}
+                  />
                 ))}
+              {showOverlay && (
+                <div className="fixed top-0 left-0 w-full h-full bg-black opacity-100">
+                  <button
+                    className="absolute top-1/2 left-20 mt-4 ml-4 cursor-pointer"
+                    onClick={handlePrevious}
+                  >
+                    <FontAwesomeIcon
+                      icon={faArrowLeft}
+                      className="text-white text-4xl"
+                    />
+                  </button>
+                  <button
+                    className="absolute top-1/2 right-20 mt-4 mr-4 cursor-pointer"
+                    onClick={handleNext}
+                  >
+                    <FontAwesomeIcon
+                      icon={faArrowRight}
+                      className="text-white text-4xl"
+                    />
+                  </button>
+                  <button
+                      className="absolute top-24 right-20 mt-4 mr-4 cursor-pointer"
+                      onClick={handleClose}
+                    >
+                      <FontAwesomeIcon icon={faTimes} className="text-white text-4xl" />
+                    </button>
+                  <div className="modal max-h-[700px]" style={modalStyle}>
+                    <img
+                      src={gallery[index].gallery.photo}
+                      alt={gallery[index].alt}
+                    />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </main>
