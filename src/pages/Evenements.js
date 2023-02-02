a43ea4e6f514c38e2713beea77e503169d409877
Agnes <almoha@gmail.com>
2023-02-02 18:01:44 +0100

    ...

--- src/pages/Evenements.js
+++ src/pages/Evenements.js
@@ -23,15 +23,15 @@ const Evenements = () => {
   return (
     <>
       <div
-        className="w-full h-screen flex flex-col"
+        className="w-full h-auto sm:h-screen flex flex-col"
         style={{ backgroundImage: `url(${bg})` }}
       >
         <Navbar />
-        <main className="flex-1 h-auto  flex  flex-col w-full mt-24 sm:mt-32 justify-center items-center">
+        <main className="flex-1 h-auto  flex  flex-col w-full mt-24 sm:mt-32 items-center">
           <h1 className="evenementTitle text-4xl text-red-600 underline mb-12">Evènements</h1>
           <div className="h-auto mb-20">
             {evenements.map((elt) => (
-              <img src={elt.evenement.href} alt="evenement" className="w-full max-h-[600px]"/>
+              <img src={elt.evenement.href} alt="evenement" className="w-11/12 mx-auto max-h-[600px] rounded-lg shadow-md shadow-white"/>
             ))}
           </div>
         </main>
