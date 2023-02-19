5305a759353f5624043af9cb66a884e94721b64a
Agnes <almoha@gmail.com>
2023-02-19 11:15:21 +0100

    classement évènements par date

--- src/pages/Evenements.js
+++ src/pages/Evenements.js
@@ -12,7 +12,9 @@ const Evenements = () => {
       const data2 = await getEvenements();
       await data2.forEach((query) =>
         result2.push({ key: query.id, evenement: query.data() })
+        
       );
+      result2.sort((a, b) => a.evenement.date.toDate().getTime() - b.evenement.date.toDate().getTime());
       setEvenements(result2);
     };
     loadData();
@@ -23,15 +25,15 @@ const Evenements = () => {
   return (
     <>
       <div
-        className="w-full h-auto sm:h-screen flex flex-col"
+        className="w-full h-auto sm:h-auto flex flex-col lg:h-screen"
         style={{ backgroundImage: `url(${bg})` }}
       >
         <Navbar />
-        <main className="flex-1 h-auto  flex  flex-col w-full mt-24 sm:mt-32 items-center">
+        <main className="flex-1 flex  flex-col w-full mt-24 sm:mt-32 items-center ">
           <h1 className="evenementTitle text-4xl text-red-600 underline mb-12">Evènements</h1>
-          <div className="h-auto mb-20">
+          <div className="h-auto mb-20 w-11/12 lg:flex lg:gap-6 lg:width-10/12 lg:flex-wrap lg:flex-row">
             {evenements.map((elt) => (
-              <img src={elt.evenement.href} key={elt.key} alt="evenement" className="w-11/12 mx-auto max-h-[600px] rounded-lg shadow-md shadow-white"/>
+              <img src={elt.evenement.href} key={elt.key} alt="evenement" className="lg:w-5/12 xl:w-4/12 2xl:w-3/12 mx-auto max-h-[600px] rounded-lg shadow-md shadow-white mb-8"/>
             ))}
           </div>
         </main>
